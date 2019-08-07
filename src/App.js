72a0a3d3a882e8c911a72a00b06f8603ed29361e
pano-skylakis <pano_skylakis@outlook.com>
2019-08-07 19:14:34 +1200

    finito

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    // Initialize state
+    state = { projects: [] };
+
+    // On load
+    componentDidMount() {
+        this.getProjects();
+    }
+
+    // Set state with our projects
+    getProjects = () => {
+        fetch('/api/projects')
+            .then(res => res.json())
+            .then(projects => this.setState({projects}));
+    }
+
+    render() {
+        const { projects } = this.state;
+
+        return (
+            <div className="App">
+
+                <h1>Hi, my name is PABLO</h1>
+                <h3>Estoy un developer</h3>
+
+                <h4>Aqui es mi projects</h4>
+
+                {
+                    projects.length ? (
+                        projects.map((project, index) => (
+                            <div className="tile" key={project.name}>
+                                <p>
+                                  <Button 
+                                    variant="contained"
+                                    href={project.html_url}>
+                                    {project.name}
+                                  </Button></p>
+                                <p>{project.description}</p>
+                            </div>
+                        ))
+                    ) : (
+                        <div>
+                            I don't have any projects
+                        </div>
+                    )
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
